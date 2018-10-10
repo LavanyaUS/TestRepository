a6433a79e51236e630374830d24f19e2339d84ee
lavan <lavan@DESKTOP-E9U7U8V>
2018-10-10 16:05:35 -0400

    Commiting by Lavanya

---
+++ Lavanya4/src/Basic_Package/Class1.java
@@ -0,0 +1,24 @@
+package Basic_Package;
+
+public class Class1 {
+	
+	public void method1()
+	{
+		System.out.println("user1");
+	}
+	
+	public void method2()
+	{
+		System.out.println("user2");	
+	}
+	
+	public static void main(String[] args)
+	{
+		Class1 obj = new Class1();
+		obj.method1();
+		obj.method2();
+	}
+	
+	
+
+}
